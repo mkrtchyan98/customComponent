d50a882c9ff3b8559fe094c756c3b34c09ba8abf
mkrtchyan98 <tigran.mkrtchyan2398@gmail.com>
2021-07-16 12:39:16 +0400

    add LineLoader and Loader custom components

---
+++ LineLoader/LineLoader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './lineLoader.scss'
+
+const LineLoader = () => {
+    return (
+        <div className="line-progress">
+
+        </div>
+    )
+}
+
+export default LineLoader
---
+++ Loader/Loader.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import './loader.scss'
+
+const Loader: React.FC = () => {
+    return (
+        <div className="line-loader">
+            <div className="loader-block">
+                <div className="sigle-slim first"></div>
+                <div className="sigle-slim second"></div>
+                <div className="sigle-slim third"></div>
+            </div>
+            <div className="loader-block">
+                <div className="sigle-slim first"></div>
+                <div className="sigle-slim second"></div>
+                <div className="sigle-slim third"></div>
+            </div>
+            <div className="loader-block">
+                <div className="sigle-slim first"></div>
+                <div className="sigle-slim second"></div>
+                <div className="sigle-slim third"></div>
+            </div>
+            <div className="loader-block">
+                <div className="sigle-slim first"></div>
+                <div className="sigle-slim second"></div>
+                <div className="sigle-slim third"></div>
+            </div>
+            <div className="loader-block">
+                <div className="sigle-slim first"></div>
+                <div className="sigle-slim second"></div>
+                <div className="sigle-slim third"></div>
+            </div>
+            <div className="loader-block">
+                <div className="sigle-slim first"></div>
+                <div className="sigle-slim second"></div>
+                <div className="sigle-slim third"></div>
+            </div>
+            <div className="loader-block">
+                <div className="sigle-slim first"></div>
+                <div className="sigle-slim second"></div>
+                <div className="sigle-slim third"></div>
+            </div>
+            <div className="loader-block">
+                <div className="sigle-slim first"></div>
+                <div className="sigle-slim second"></div>
+                <div className="sigle-slim third"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
